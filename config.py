9c3dafb5b11a9970a0c5cba49c12130bb6e699bf
git <philip201@gmail.com>
2021-04-23 18:32:10 +0300

    Set default config

---
+++ config.py
@@ -0,0 +1,9 @@
+# Основной конифг Бота CuteURL
+URL_CUTTLY = "https://cutt.ly/api/api.php"
+API_CUTTLY = "<api key>"
+
+# API Bit.ly l: o_35htgecvam
+API_BITLY = '<api key>'
+
+# API bot telegram
+API_BOT = "<api key>"
